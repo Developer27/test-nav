2a9889ff3431c84f38bbc336e527d8b0812c8759
dmitry <dyivanov32@gmail.com>
2026-05-08 13:58:10 +0300

    fix styles

--- src/components/icons/DropDownArrowDownIcon.jsx
+++ src/components/icons/DropDownArrowDownIcon.jsx
@@ -7,7 +7,7 @@ const DropDownArrowDownIcon = ({ className, ...props }) => {
         d="M3 5.5L5.27875 7.77875C5.67709 8.17709 6.32291 8.17709 6.72125 7.77875L9 5.5"
         stroke="currentColor"
         strokeOpacity="0.4"
-        strokeWidth="0.75"
+        strokeWidth="1.25"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
